ced16d11d0a94cdbc75e62dc1a09a3d69b06ca21
ezequielbr <ezequielbravo00@gmail.com>
2024-02-20 19:18:08 -0300

    first commit

---
+++ src/main/java/com/Car4All/Proyecto/Car4AllApplication.java
@@ -0,0 +1,13 @@
+package com.Car4All.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Car4AllApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/com/Car4All/Proyecto/Car4AllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Car4All.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Car4AllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
