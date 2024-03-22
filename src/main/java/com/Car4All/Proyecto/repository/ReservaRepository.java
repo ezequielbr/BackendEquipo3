4950dbb97923b539bd037da4a0df9cd5ee236a42
ezequielbr <ezequielbravo00@gmail.com>
2024-03-22 19:31:44 -0300

    Bug Fixing

--- src/main/java/com/Car4All/Proyecto/repository/ReservaRepository.java
+++ src/main/java/com/Car4All/Proyecto/repository/ReservaRepository.java
@@ -2,10 +2,11 @@ package com.Car4All.Proyecto.repository;
 
 import com.Car4All.Proyecto.entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.Optional;
-
+@Repository
 public interface ReservaRepository extends JpaRepository<Reserva,Long> {
     Optional<Reserva> findByFechaInicio(LocalDate fechaInicio);
     Optional<Reserva> findByFechaFin(LocalDate fechaFin);
--- src/main/java/com/Car4All/Proyecto/repository/ValoracionRepository.java
+++ src/main/java/com/Car4All/Proyecto/repository/ValoracionRepository.java
@@ -2,6 +2,8 @@ package com.Car4All.Proyecto.repository;
 
 import com.Car4All.Proyecto.entity.Valoracion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ValoracionRepository extends JpaRepository<Valoracion,Long> {
 }
